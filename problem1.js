8aa2ba577d88343906ed71c2c73f7bbc99661d2f
Anthony Trinh <anthony.trinh08>
2015-10-22 13:53:01 -0700

    Merged all changes

--- problem1.js
+++ problem1.js
@@ -2,11 +2,8 @@
 //added comment A
 //added comment B
 //added comment D
-<<<<<<< HEAD
 //add comment F
-=======
-//add comment E
->>>>>>> parent of 9d0c65a... Revert "Add comment E"
+
 
 // checking out old version, adding comment C
 
