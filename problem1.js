e00f7123a3f7cca22bbb935be16566ad31008be9
Anthony Trinh <anthony.trinh08>
2015-10-28 11:32:19 -0700

    on newBranch.  did some work.

--- problem1.js
+++ problem1.js
@@ -8,7 +8,7 @@
 //now on goback5 branch. doing some work.
 //now on branch6, doing some feature6.
 
-
+//just checked out newBranch.  doing some work on it.  will save and commit, and push.
 
 function factorial(num) {
 	if (num === 1) {return 1}
